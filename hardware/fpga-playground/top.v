b64c91d68867ff3117f0bb9b326d83ea05a91230
Andy18650 <1145662805@qq.com>
2025-06-04 17:24:26 +0800

    uart testing

--- hardware/fpga-playground/top.v
+++ hardware/fpga-playground/top.v
@@ -1,101 +1,35 @@
-`define	kFofE_HFOSC_CLOCK_DIVIDER_FOR_1Hz	100000
-
-module top(led,reset);
-	output		led;
-	input		reset;
-
+module top(
+	output		led,
+	input		uart_rx,
+	output 		uart_tx,
+);
 	wire		clk;
-	reg		LEDstatus = 1;
-	reg [31:0]	count = 0;
-
-	reg [31:0] addr_src1;
-	reg [31:0] addr_src2;
-
-	initial begin
-		addr_src1 = 12345;
-		addr_src2 = 67890;
-	end
-
-	wire [31:0] rddata;
-	wire [15:0] outdata;
-
-	wire [3:0] select = count[18:15];
+	reg [12:0]	count = 0;
+	wire [9:0] data = 10'b1010101010;
 	/*
 	 *	Creates a 48MHz clock signal from
 	 *	internal oscillator of the iCE40
 	 */
-	SB_HFOSC OSCInst0 (
+	SB_HFOSC #(.CLKHF_DIV("0b00")) OSCInst0 (
 		.CLKHFPU(1'b1),
 		.CLKHFEN(1'b1),
 		.CLKHF(clk)
 	);
 
-	
+	reg [3:0] bit_counter;
 
-	defparam OSCInst0.CLKHF_DIV = "0b01";
-	/*
-	 *	Blinks LED at approximately 1Hz. The constant kFofE_CLOCK_DIVIDER_FOR_1Hz
-	 *	(defined above) is calibrated to yield a blink rate of about 1Hz.
-	 */
-	wire [18:0] data_addr;
-	// agu agu(
-	// 	.addr1(addr_src1),
-	// 	.addr2(addr_src2),
-	// 	.addr_out(data_addr)
-	// );
-
-	wire [31:0] addr = addr_src1 + count;//count[0] ? //something
-	// count[15:2] : // something
-	// data_addr[18:5];
-
-    // SB_SPRAM256KA blk0(
-    //     .CLOCK(clk),
-    //     .ADDRESS(addr),
-    //     .DATAIN(count[15:0]),
-    //     .DATAOUT(rddata[15:0]),
-    //     .MASKWREN(4'b1),
-    //     .WREN(1'b0),
-    //     .CHIPSELECT(1'b1),
-    //     .STANDBY(1'b0),
-    //     .SLEEP(1'b0),
-    //     .POWEROFF(1'b1)
-	// );
-    // SB_SPRAM256KA blk1(
-    //     .CLOCK(clk),
-    //     .ADDRESS(count),
-    //     .DATAIN(count[15:0]),
-    //     .DATAOUT(rddata[31:16]),
-    //     .MASKWREN(4'b1),
-    //     .WREN(1'b0),
-    //     .CHIPSELECT(1'b1),
-    //     .STANDBY(1'b0),
-    //     .SLEEP(1'b0),
-    //     .POWEROFF(1'b1)
-	// );
-	// wire [15:0] nextcount = outdata + 1;
-	// // assign outdata = select[2] ? rddata[31:16] : rddata[15:0];
-	// assign outdata = rddata[15:0];
-	// assign outdata = {24'b0,select[2] ? (select[3] ? rddata[31:24] : rddata[15:8]) : (select[3] ? rddata [23:16] : rddata[7:0])};
-	always @(posedge clk or posedge reset) begin
-		// if (count > `kFofE_HFOSC_CLOCK_DIVIDER_FOR_1Hz) begin
-		// 	LEDstatus <= !LEDstatus;
-		// 	count <= 12345;
-		// end
-		// else begin
-		if(reset)begin
-			count <=0;
-			addr_src2 <=0;
-			addr_src1 <=0;
+	always @(posedge clk) begin
+		if (count > 13'd5000)begin
+			bit_counter <= bit_counter +1;
+			count <= 0;
 		end else begin
 			count <= count+1;
-			addr_src2 <= ~addr_src1;
-			addr_src1 <= addr_src2;
 		end
-		// end
 	end
+	assign uart_tx = (bit_counter > 9) ? data[9] : data[bit_counter];
 
 	/*
 	 *	Assign output led to value in LEDstatus register
 	 */
-	assign	led = addr[31];
+	assign	led = (bit_counter > 9) ? data[9] : data[bit_counter];
 endmodule
